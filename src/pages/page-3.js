3a304c84c8a0f1e784fda702573a45f664c96fbc
JimHui12 <jianghua.hui@uq.net.au>
2020-11-17 18:17:13 +1000

    adding all new blog code for gatsby setup

---
+++ src/pages/page-3.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThirddPage = () => (
+  <Layout>
+    <SEO title="Page three" />
+    <h1>Hi from the third page</h1>
+  </Layout>
+)
+
+export default ThirddPage
